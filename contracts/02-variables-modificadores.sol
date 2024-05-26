147d1366124c6505f48b0e267226429f162556a2
Irwing <86335744+Irwingduran@users.noreply.github.com>
2024-05-26 14:52:48 -0600

    coomit

--- contracts/02-variables-modificadores.sol
+++ contracts/02-variables-modificadores.sol
@@ -11,5 +11,29 @@ contract variablesModificadores {
     int256 c;
     int8 public d = -32;
     int e = 65;
+
+    //variable string 
+    string str; 
+    string public str_public = "Esto es publico";
+    string private str_private = "Esto es privado"; 
+
+    //variable boolean
+    bool boolean;
+    bool public boolean_true = true;
+    bool private boolean_false = false;
+
+    //variable bytes
+    bytes32 firts_bytes;
+    bytes4 second_bytes;
+    bytes1 byte_1; 
+
+    // algoritmo de hash 
+    bytes32 public hashing = keccak256(abi.encodePacked("hola", "Juan"));
+
+    // variable address
+    address my_address;
+    address public address1 = 0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB;
+    address public address2 = msg.sender; //la dirección de la persona que está ejecutando esta función
+
     
 }
